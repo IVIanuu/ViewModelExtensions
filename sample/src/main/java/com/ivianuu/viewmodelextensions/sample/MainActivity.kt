fe10d5b0cd0d1dae3c959b41373af7735ae36282
IVIanuu <IVIanuu@gmail.com>
2017-11-04 15:13:34 +0100

    Exchanged key and factory params to allow nicer syntax

--- sample/src/main/java/com/ivianuu/viewmodelextensions/sample/MainActivity.kt
+++ sample/src/main/java/com/ivianuu/viewmodelextensions/sample/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.ivianuu.viewmodelextensions.sample
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ivianuu.viewmodelextensions.bindViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel by bindViewModel<MainViewModel>()
+    private lateinit var factory: ViewModelProviders.DefaultFactory
+
+    private val viewModel by bindViewModel<MainViewModel> { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ viewmodelextensions/src/main/java/com/ivianuu/viewmodelextensions/ViewModelExt.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.viewmodelextensions
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+
+/**
+ * Returns the viewmodel
+ */
+inline fun <reified T : ViewModel> FragmentActivity.getViewModel(key: String? = null,
+                                                                 factory: ViewModelProvider.Factory? = null) : T {
+    val provider = if (factory != null) {
+        ViewModelProviders.of(this, factory)
+    } else{
+        ViewModelProviders.of(this)
+    }
+
+    return if (key != null) {
+        provider.get(key, T::class.java)
+    } else{
+        provider.get(T::class.java)
+    }
+}
+
+/**
+ * Lazily binds the view model
+ */
+inline fun <reified T : ViewModel> FragmentActivity.bindViewModel(key: String? = null,
+                                                                  factory: ViewModelProvider.Factory? = null)
+        = lazy(LazyThreadSafetyMode.NONE) { getViewModel<T>(key, factory) }
+
+/**
+ * Lazily binds the view model
+ * This method is useful if your factory is a late init var
+ */
+inline fun <reified T : ViewModel> FragmentActivity.bindViewModel(key: String? = null,
+                                                                  crossinline factoryProvider: () ->  ViewModelProvider.Factory)
+        = lazy(LazyThreadSafetyMode.NONE) { getViewModel<T>(key, factoryProvider()) }
+
+/**
+ * Returns the viewmodel
+ */
+inline fun <reified T : ViewModel> Fragment.getViewModel(key: String? = null,
+                                                         factory: ViewModelProvider.Factory? = null) : T {
+    val viewModelProvider = if (factory != null) {
+        ViewModelProviders.of(this, factory)
+    } else{
+        ViewModelProviders.of(this)
+    }
+
+    return if (key != null) {
+        viewModelProvider.get(key, T::class.java)
+    } else{
+        viewModelProvider.get(T::class.java)
+    }
+}
+
+/**
+ * Lazily binds the view model
+ */
+inline fun <reified T : ViewModel> Fragment.bindViewModel(key: String? = null,
+                                                          factory: ViewModelProvider.Factory? = null)
+        = lazy(LazyThreadSafetyMode.NONE) { getViewModel<T>(key, factory) }
+
+/**
+ * Lazily binds the view model
+ * This method is useful if your factory is a late init var
+ */
+inline fun <reified T : ViewModel> Fragment.bindViewModel(key: String? = null,
+                                                          crossinline factoryProvider: () ->  ViewModelProvider.Factory)
+        = lazy(LazyThreadSafetyMode.NONE) { getViewModel<T>(key, factoryProvider()) }
